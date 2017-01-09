9bd7fb2279da29d8a53a72054885eb2a6f035f04
Adriano Soares <gsoares.adriano@gmail.com>
2017-01-08 23:50:20 -0200

    MVP done

--- index.js
+++ index.js
@@ -1,18 +1,43 @@
 #!/usr/bin/env node
 
+//https://torrentapi.org/apidocs_v2.txt
+
+let request = require('superagent')
 let program = require('commander');
 
-const baseUrl = "https://rarbg.to/torrents.php?search=<search>&category=1;<categories>";
+const baseUrl = 'https://torrentapi.org/pubapi_v2.php';
+
+let searchTorrent = (token, search, params) => {
+    request
+      .get(baseUrl)
+      .query({ mode: 'search', search_string: search, category: '18', sort: 'seeders' , token: token, app_id : 'rarbg-cli' })
+      .end((err, res) => {
+        if (res.statusCode === 200) {
+          if (res.body.torrent_results) {
+            console.log(res.body.torrent_results[0].download)
+          } else {
+            console.log ('Not found :(')
+          }
+          
+        } else {
+          console.log ('Error')
+        }
+    });
+};
 
 program
   .arguments('<search>')
   .action((search) => {
-      let categories = "18;41"
-      let url = baseUrl
-                  .replace("<search>", encodeURI(search))
-                  .replace("<categories>", categories)
-
-      console.log(url)
+      request
+        .get(baseUrl)
+        .query({ get_token: 'get_token', app_id : 'rarbg-cli' })
+        .end((err, res) => {
+          if (res.statusCode === 200) {
+            searchTorrent (res.body.token, search, null) 
+          } else {
+            console.log ('Error')
+          }
+        });
   })
   .parse(process.argv);
 
